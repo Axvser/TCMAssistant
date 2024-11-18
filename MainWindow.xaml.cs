a3e47db3445c5fa62439b958a6426975492cee4d
冯 <2789083329@qq.com>
2024-11-18 16:55:06 +0800

    主界面搭建（原型

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TCMAssistant.Page;
 
 namespace TCMAssistant
 {
@@ -17,6 +18,80 @@ namespace TCMAssistant
         public MainWindow()
         {
             InitializeComponent();
+            NowPage.Navigate(typeof(ConfigPage));
+        }
+
+        private static TransitionBoard<Border> _noselected = Transition.CreateBoardFromType<Border>()
+            .SetProperty(x => x.Background, Brushes.Gray)
+            .SetProperty(x => x.Opacity, 0.6)
+            .SetParams((x) =>
+            {
+                x.Duration = 0.2;
+            });
+        private static TransitionBoard<Border> _selected = Transition.CreateBoardFromType<Border>()
+            .SetProperty(x => x.Background, Brushes.Cyan)
+            .SetProperty(x => x.Opacity, 1)
+            .SetParams((x) =>
+            {
+                x.Duration = 0.5;
+            });
+
+        private int _pagetype = 1;
+        public int PageType//当前页面标号
+        {
+            get => _pagetype;
+            set
+            {
+                if (value != _pagetype)
+                {
+                    PageLightChange(_pagetype, value);
+                    _pagetype = value;
+                }
+            }
+        }
+
+        private void PageLightChange(int last, int next)
+        {
+            switch (last)
+            {
+                case 1:
+                    B1.BeginTransition(_noselected);
+                    break;
+                case 2:
+                    B2.BeginTransition(_noselected);
+                    break;
+                case 3:
+                    B3.BeginTransition(_noselected);
+                    break;
+            }
+            switch (next)
+            {
+                case 1:
+                    B1.BeginTransition(_selected);
+                    break;
+                case 2:
+                    B2.BeginTransition(_selected);
+                    break;
+                case 3:
+                    B3.BeginTransition(_selected);
+                    break;
+            }
+        }
+
+        private void Config_Click(object sender, MouseButtonEventArgs e)
+        {
+            NowPage.Navigate(typeof(ConfigPage));
+            PageType = 1;
+        }
+        private void HSV_Click(object sender, MouseButtonEventArgs e)
+        {
+            NowPage.Navigate(typeof(HSVPage));
+            PageType = 2;
+        }
+        private void AI_Click(object sender, MouseButtonEventArgs e)
+        {
+            NowPage.Navigate(typeof(AIPage));
+            PageType = 3;
         }
     }
 }
---
+++ Service/HSV.cs
@@ -0,0 +1,41 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCMAssistant.Service
+{
+    public class HSV : IAssistant
+    {
+        public HSV() { }
+        public HSV(float h, float s, float v)
+        {
+            H = h;
+            S = s;
+            V = v;
+        }
+
+        /// <summary>
+        /// 色相
+        /// </summary>
+        public float H { get; set; } = 0f;
+        /// <summary>
+        /// 饱和度
+        /// </summary>
+        public float S { get; set; } = 0f;
+        /// <summary>
+        /// 亮度
+        /// </summary>
+        public float V { get; set; } = 0f;
+
+        public object HSVAssistant(HSV value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
